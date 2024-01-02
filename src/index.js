553779efd3c5375721b4dcb3a3f1f42be636b32f
lerato-ledwaba <154332864+lerato-ledwaba@users.noreply.github.com>
2024-01-02 17:35:49 +0200

    Updated html and css files

--- src/index.js
+++ src/index.js
@@ -53,6 +53,22 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay (timestamp){
+let date= new Date(timestamp * 1000);
+let days = [
+"Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",];
+return days[date.getDay()]
+}
+
+
+
+
 function getForecast(city) {
  let apiKey = "3cetfbf0a2fo789eb1646460eaeb53d1";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -66,25 +82,27 @@ function displayForecast(response){
 
 let days =["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 let forecastHtml="";
-
-days.forEach(function(day){
+response.data.daily.forEach(function(day, index){
+  if (index < 6)
 forecastHtml =
-forecastHtml +
- `
+  forecastHtml +
+  `
 <div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
-<div class="weather-forecast-icon">⛅️</div>
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+<img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
 <div class="weather-forecast-temperatures">
 <div class="weather-forecast-temperature">
-<strong>15°</strong>
+<strong>$${Math.round(day.temperature.maximum)}°</strong>
 </div>
-<div class="weather-forecast-temperature">9°</div>
+<div class="weather-forecast-temperature"> ${Math.round(
+    day.temperature.minimum
+  )}°</div>
 </div>
 </div>
 `;
-
-});
 }
+});
+
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
 
@@ -96,4 +114,3 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
 
-displayForecast();
