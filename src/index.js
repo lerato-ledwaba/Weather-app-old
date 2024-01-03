67c62ae39c4162441ee2fb4ca3f7e4de0dad6ba4
lerato-ledwaba <154332864+lerato-ledwaba@users.noreply.github.com>
2024-01-03 10:05:46 +0200

    Updated html,css and js files

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML= `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   
-getForecast(response.data.city);
 
 }
 
@@ -53,61 +52,6 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-function formatDay (timestamp){
-let date= new Date(timestamp * 1000);
-let days = [
-"Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",];
-return days[date.getDay()]
-}
-
-
-
-
-function getForecast(city) {
- let apiKey = "3cetfbf0a2fo789eb1646460eaeb53d1";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayForecast);
-}
-
-
-
-function displayForecast(response){
-  console.log(response.data);
-
-let days =["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-let forecastHtml="";
-response.data.daily.forEach(function(day, index){
-  if (index < 6)
-forecastHtml =
-  forecastHtml +
-  `
-<div class="weather-forecast-day">
-<div class="weather-forecast-date">${formatDay(day.time)}</div>
-<img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
-<div class="weather-forecast-temperatures">
-<div class="weather-forecast-temperature">
-<strong>$${Math.round(day.temperature.maximum)}°</strong>
-</div>
-<div class="weather-forecast-temperature"> ${Math.round(
-    day.temperature.minimum
-  )}°</div>
-</div>
-</div>
-`;
-}
-});
-
-let forecastElement = document.querySelector("#forecast");
-forecastElement.innerHTML = forecastHtml;
-
-
-
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
